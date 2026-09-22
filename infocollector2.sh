e2411660ee7649e2c3c44001635a110d80c3be9c
vkpichu <94012682+vkpichu@users.noreply.github.com>
2026-09-23 02:06:28 +0400

    infocollector2.sh

--- infocollector2.sh
+++ infocollector2.sh
@@ -3,7 +3,7 @@
 . /etc/profile.d/vsenv.sh
 #
 # Script: infocollector2.sh
-# last update 20260917
+# last update 20260923
 # Script by : VK Prasad, Professional Services Consultant
 # Copyrights:  Check Point Software Technologies LTD.
 #
